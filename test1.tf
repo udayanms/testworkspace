5d8bf6da5847379653056cac16dbbc6122092966
udayanms <52168703+udayanms@users.noreply.github.com>
2020-08-06 23:00:20 +0000

    Create test1.tf

---
+++ test1.tf
@@ -0,0 +1,10 @@
+provider "tfe" {
+#  hostname = "${var.hostname}"
+#  token    = "${var.token}"
+  version  = "~> 0.15.0"
+}
+
+resource "tfe_workspace" "test" {
+  name         = "my-workspace-name"
+  organization = "TFE-LZ"
+}
