efe0c69e623027582ce5bda716e1caa3b10768f5
udayanms <52168703+udayanms@users.noreply.github.com>
2020-08-07 01:00:59 +0000

    Update test1.tf

--- test1.tf
+++ test1.tf
@@ -1,10 +1,39 @@
+locals {
+
+   repo_name = "my-workspace-name"
+}
+
 provider "tfe" {
+  alias = "tfe1"
   hostname = var.tf_hostname
- token    = var.token
+  token    = var.token
   version  = "~> 0.15.0"
+}
+
+
+
+# Configure the GitHub Provider
+provider "github" {
+  alias = "github1"
+  token        = var.github_token
+  organization = var.github_organization
+}
+
+# Add a user to the organization
+resource "github_repository" "example" {
+  name         = "example"
+  description  = "My awesome codebase"
+  provider     = github.github1
+#  private = true
+
+#  template {
+#    owner = "github"
+#    repository = "terraform-module-template"
+#  }
 }
 
 resource "tfe_workspace" "test" {
   name         = "my-workspace-name"
   organization = "TFEDemo"
+  provider = tfe.tfe1
 }
